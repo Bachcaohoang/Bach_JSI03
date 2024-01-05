8db949bbd822d2abf6d9b6719acb02e68d915111
Bachs-JSA05 <bachcaoryan1234@gmail.com>
2024-01-05 22:05:20 +0700

    KTR

--- Buổi học/Buoi3/Buoi3.js
+++ Buổi học/Buoi3/Buoi3.js
@@ -1,6 +1,22 @@
 // class cần một cái tên chung ko quá cụ thể như student,Sport
 // constructor khi đặc các tên thì các đó kèm với thuộc tính của nó
 // this là dùng để lấy các Object  trong một đối tượng và khi khai báo cái Object thì sẽ đi kèm với thuộc tính Object
+// 1. Tính trừu tượng:
+
+//    - Các dữ liệu và phương thức có liên quan với nhau được đóng gói thành các lớp để tiện cho việc quản lý và sử dụng.
+//    - Che giấu một số thông tin và chi tiết cài đặt nội bộ để bên ngoài không thể nhìn thấy
+
+// 2. Tính đóng gói:
+//    - Tổng quát hóa một cái gì đó lên, không cần chú ý chi tiết bên trong. Nó không màng đến chi tiết bên trong là gì và người ta vẫn hiểu nó mỗi khi nghe về nó
+
+// 3. Tính kế thừa:
+
+//    - Nó cho phép xây dựng một lớp mới dựa trên các định nghĩa của lớp đã có
+//    - Các lớp con khỏi phải định nghĩa lại, ngoài ra có thể mở rộng các thành phần kế thừa và bổ sung thêm các thành phần mới.
+
+// 4. Tính đa hình:
+//    - Hai hoặc nhiều lớp có những phương thức giống nhau nhưng có thể thực thi theo những cách thức khác nhau
+//    - VD: lưu trữ thông tin trên cloud
 class Sport{
     constructor(point,age,time){
         (this.point = point),(this.age = age),(this.time = time);
---
+++ Ktr/KTR.js
@@ -0,0 +1,32 @@
+// Câu 9
+let  m = [1,2,3,4,5,6,"hh" ,"9",80,100];
+let mSquared = m.filter(item => typeof item === 'number').map(item => Math.pow(item, 2));
+console.log(mSquared);
+// câu10
+let str = "High knowledge, high return";
+let arr = str.split(" ").filter(item => item !== "").map(item => item.toLowerCase());
+console.log(arr);
+// câu11
+class Employee{
+    constructor(name, salary){
+        this.name = name, this.salary = salary
+    }
+displayInfo(){
+            console.log(this.name + " lương: " + this.salary)
+        }
+}
+
+class Manager extends Employee{
+    constructor(name, salary, department){
+       super(name,salary), this.department = department;
+    }
+    displayInfo(){
+        console.log("Tên: " + this.name + ", lương: " + this.salary + ", phòng ban: "+ this.department)
+        // console.log(" daaddad "+ Employee.salary);
+    }
+}
+var nhut = new Employee("nhut", 1000000)
+var lam = new Manager("abc",34400000,"Tài chính kế toán")
+
+lam.displayInfo();
+nhut.displayInfo();
